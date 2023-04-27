537709f4a7fddda9595bf5513bec121d4e9932b8
Malik Shanaah <95984769+malikshanaah1999@users.noreply.github.com>
2023-04-27 13:34:25 +0300

    Update byHand.sv

--- encoder/byHand.sv
+++ encoder/byHand.sv
@@ -1,11 +1,11 @@
-module conv_encoder(input clk, input reset_n, input [7:0] data_in, output reg [15:0] data_out);
+module conv_encoder(input clk, input reset_n, input [7:0] data_in, output reg [15:0] encoded_data);
 
 parameter g1 = 8'b10110111; // Generator polynomial for Encoder 1
 parameter g2 = 8'b11110001; // Generator polynomial for Encoder 2
 
 reg [7:0] state1, state2;
 wire [7:0] next_state1, next_state2;
-wire output1, output2;
+wire out1, out2;
 
 // Encoder 1
 conv_encoder_1 encoder_1 (
@@ -14,7 +14,7 @@ conv_encoder_1 encoder_1 (
   .reset_n(reset_n),
   .state(state1),
   .next_state(next_state1),
-  .output(output1)
+  .out(out1)
 );
 
 // Encoder 2
@@ -24,19 +24,19 @@ conv_encoder_2 encoder_2 (
   .reset_n(reset_n),
   .state(state2),
   .next_state(next_state2),
-  .output(output2)
+  .out(out2)
 );
 
-// Output MUX
+// Mux the outputs of the two encoders based on input bit 0
 always @(*) begin
   if (data_in[0]) begin
-    data_out = {output2, output1};
+    encoded_data = {out2, out1};
   end else begin
-    data_out = {output1, output2};
+    encoded_data = {out1, out2};
   end
 end
 
-// State update
+// Update the encoder states
 always @(posedge clk or negedge reset_n) begin
   if (!reset_n) begin
     state1 <= 8'b00000000;
@@ -48,7 +48,7 @@ always @(posedge clk or negedge reset_n) begin
 end
 
 // Encoder 1
-module conv_encoder_1(input clk, input [7:0] data_in, input reset_n, input [7:0] state, output reg [7:0] next_state, output reg output);
+module conv_encoder_1(input clk, input [7:0] data_in, input reset_n, input [7:0] state, output reg [7:0] next_state, output reg out);
 
 reg [7:0] state_reg;
 
@@ -66,11 +66,11 @@ always @(posedge clk) begin
   case (state_reg)
     8'b00000000: begin
       next_state = 8'b10000000;
-      output = data_in[7] ^ data_in[6] ^ data_in[5] ^ data_in[3] ^ data_in[2] ^ data_in[0];
+      out = data_in[7] ^ data_in[6] ^ data_in[5] ^ data_in[3] ^ data_in[2] ^ data_in[0];
     end
     default: begin
       next_state = {state_reg[6:0], data_in[7]};
-      output = state_reg[7] ^ state_reg[6] ^ state_reg[5] ^ state_reg[3] ^ state_reg[2] ^ state_reg[0];
+      out = state_reg[7] ^ state_reg[6] ^ state_reg[5] ^ state_reg[3] ^ state_reg[2] ^ state_reg[0];
     end
   endcase
 end
@@ -78,7 +78,7 @@ end
 endmodule
 
 // Encoder 2
-module conv_encoder_2(input clk, input [7:0] data_in, input reset_n, input [7:0] state, output reg [7:0] next_state, output reg output);
+module conv_encoder_2(input clk, input [7:0] data_in, input reset_n, input [7:0] state, output reg [7:0] next_state, output reg out);
 
 reg [7:0] state_reg;
 
@@ -93,14 +93,16 @@ end
 
 // Encoder logic
 always @(posedge clk) begin
-  case (state_reg)
-    8'b00000000: begin
-      next_state = 8'b10000000;
-     
-  output = data_in[7] ^ data_in[6] ^ data_in[4] ^ data_in[2] ^ data_in[0];
+case (state_reg)
+8'b00000000: begin
+next_state = 8'b10000000;
+out = data_in[7] ^ data_in[6] ^ data_in[4] ^ data_in[2] ^ data_in[0];
 end
 default: begin
-  next_state = {state_reg[6:0], data_in[7]};
-  output = state_reg[7] ^ state_reg[6] ^ state_reg[4] ^ state_reg[2] ^ state_reg[0];
+next_state = {state_reg[6:0], data_in[7]};
+out = state_reg[7] ^ state_reg[6] ^ state_reg[4] ^ state_reg[2] ^ state_reg[0];
 end
-      endmodule 
+endcase
+end
+
+endmodule
